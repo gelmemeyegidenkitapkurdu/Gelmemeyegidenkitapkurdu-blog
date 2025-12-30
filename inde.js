b2a5a4b69c5b319e1e739117725f5e0b221d704e
gelmemeyegidenkitapkurdu <gelmemeyegidenkitapkurdu@gmail.com>
2025-12-30 19:21:40 +0300

    Update inde.js

--- inde.js
+++ inde.js
@@ -2,7 +2,7 @@ import supabase from '../lib/supabaseClient'
 
 export async function getStaticProps() {
   const { data, error } = await supabase
-  .from('blog_posts')
+  .from('public.blog_posts')
   .select('id, title, content')
   .order('created_at', { ascending: false })
 
