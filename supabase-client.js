cfae8e56764c23672e6014861c828dab71c7eeaa
gelmemeyegidenkitapkurdu <gelmemeyegidenkitapkurdu@gmail.com>
2025-12-30 18:56:57 +0300

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -3,6 +3,13 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error('❌ Supabase ENV eksik:', {
+    NEXT_PUBLIC_SUPABASE_URL: supabaseUrl,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: supabaseAnonKey,
+  })
+}
+
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export default supabase
