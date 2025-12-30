a1a30adc177fecf06de8b7fa26896027c6e13ffa
gelmemeyegidenkitapkurdu <gelmemeyegidenkitapkurdu@gmail.com>
2025-12-30 19:22:56 +0300

    Fix table name in getStaticProps function

--- inde.js
+++ inde.js
@@ -2,7 +2,7 @@ import supabase from '../lib/supabaseClient'
 
 export async function getStaticProps() {
   const { data, error } = await supabase
-  .from('public.blog_posts')
+  .from('blog_posts')
   .select('id, title, content')
   .order('created_at', { ascending: false })
 
