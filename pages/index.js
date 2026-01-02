ba562b2dbcaa605fa97f70270fbc81403f82d9ef
gelmemeyegidenkitapkurdu <gelmemeyegidenkitapkurdu@gmail.com>
2026-01-02 20:04:55 +0300

    Add pages directory and homepage

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Gelmemeyegidenkitapkurdu</h1>
+      <p>Site başarıyla çalışıyor 🎉</p>
+    </div>
+  )
+}
